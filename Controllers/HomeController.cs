783f2e3baf9c26bc915ff4c0b02f493a78c1e09a
ekurniawan <erick.kurniawan@gmail.com>
2019-03-12 11:24:44 +0700

    menambahkan view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SampleASPCore.Controllers {
     public class HomeController : Controller{
+        public IActionResult Index(){
+            return View();
+        }
 
+        public string About(){
+            return "Hello from About";
+        }
     }
 }
